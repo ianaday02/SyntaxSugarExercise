6551872baee37cf589bb78443b21d072b84151dd
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-10 17:41:52 -0500

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4;
+            //string response;
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + “greater than or equal to nine”;
+            //}
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.Write(response);
+
+        }
+    }
+}
+
